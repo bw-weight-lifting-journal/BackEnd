212953bcaf4de696f72bddc847516a11066cf6ea
Matt Locklin <mattlocklin@live.co.uk>
2019-07-31 16:51:26 +0100

    Added create/update/delete exercise endpoints and db connections. tested workinG

--- exercise/exercise-helpers.js
+++ exercise/exercise-helpers.js
@@ -5,14 +5,39 @@ function findExercise() {
         .select('id', 'exerciseName');
 }
 
-function findExerciseById(id) {
-    return db('arms')
+function findExerciseById(name, id) {
+    return db(name)
         .select('id', 'exerciseName')
         .where({ 'id': id })
         .first();
 }
 
+function insertExercise(name, newExercise) {
+    return db(name)
+        .returning(['id', 'exerciseName'])
+        .insert(newExercise);
+    
+}
+
+function updateExercise(name, id, updatedExercise, ) {
+    return db(name)
+        .where({ 'id': id })
+        .update({ 'exerciseName': updatedExercise.exerciseName })
+        .returning(['id', 'exerciseName']);
+    
+}
+
+function deleteExercise(name, id) {
+    return db(name)
+        .where({ 'id': id })
+        .del()
+        .returning('id', 'name');
+}
+
 module.exports = {
     findExercise,
-    findExerciseById
+    findExerciseById,
+    insertExercise,
+    updateExercise,
+    deleteExercise
 };
--- exercise/exercise-router.js
+++ exercise/exercise-router.js
@@ -2,21 +2,58 @@ const router = require('express').Router();
 const db = require('./exercise-helpers');
 const restricted = require('../middleware/restricted-route');
 
-router.get('/arms', restricted, (req, res) => {
-    db.findExercise()
-        .then(users => {
-            res.json(users);
+router.get('/:name', restricted, (req, res) => {
+    db.findExercise(req.params.name)
+        .then(exercises => {
+            res.status(200).json(exercises);
         })
-        .catch(err => res.send(err));
+        .catch(err => {
+            res.status(500).json({ Error: 'Internal Server Error' });
+        });
 });
 
-router.get('/arms/:id', restricted, (req, res) => {
-    db.findExerciseById(req.params.id)
-        .then(users => {
-            res.json(users);
+router.get('/:name/:id', restricted, (req, res) => {
+    db.findExerciseById(req.params.name, req.params.id)
+        .then(exercise => {
+            res.status(200).json(exercise);
         })
-        .catch(err => res.send(err));
+        .catch(err => {
+            res.status(500).json({ Error: 'Internal Server Error' });
+        });
+    
 });
+router.post('/:name', restricted, (req, res) => {
+    db.insertExercise(req.params.name, req.body)
+        .then(exercise => {
+            res.status(201).json(exercise);
+        })
+        .catch(err => {
+            res.status(500).json({ Error: 'Internal Server Error' });
+        });
+});
+
+router.put('/:name/:id', restricted, (req, res) => {
+    console.log(req.body);
+    db.updateExercise(req.params.name, req.params.id, req.body)
+        .then(exercise => {
+            res.status(200).json(exercise);
+        })
+        .catch(err => {
+            res.status(500).json({ Error: 'Internal Server Error' });
+        });
+});
+
+router.delete('/:name/:id', restricted, (req, res) => {
+    db.deleteExercise(req.params.name, req.params.id)
+        .then(exercise => {
+            res.status(204).json({Deleted: 'Exercise Deleted, Have a nice day'});
+        })
+        .catch(err => {
+            res.status(500).json({ Error: 'Internal Server Error' });
+        });
+});
+    
+
 
 module.exports = router;
 
